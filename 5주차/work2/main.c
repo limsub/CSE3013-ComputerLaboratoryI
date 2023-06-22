fd8d7829b9f2683eb011c67ee9b1f751839f6ced
limsub <99518799+limsub@users.noreply.github.com>
2023-06-22 20:56:10 +0900

    Add files via upload

---
+++ 5주차/work2/main.c
@@ -0,0 +1,17 @@
+int main() {
+
+    printf("hello world");
+
+    printf("hello world, it's testbranch1");
+}
+
+void add(int x, int y) {
+
+    printf("%d\n", x + y);
+}
+
+
+void multiple(int x, int y) {
+
+    printf("%d", x * y);
+}
